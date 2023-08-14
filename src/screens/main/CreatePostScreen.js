618643209f9e38644ce88e1b979c5c29a17b9c8f
TurtlePower <agnkvdm@gmail.com>
2023-08-14 14:45:48 +0300

    add create post buttons logic

--- src/screens/main/CreatePostScreen.js
+++ src/screens/main/CreatePostScreen.js
@@ -1,48 +1,86 @@
-import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 
 export default function CreatePostsScreen() {
+  const [photoName, setPhotoName] = useState('');
+  const [photoLocation, setPhotoLocation] = useState('');
+
+  const isReadyToPost = !!photoName && !!photoLocation;
+
+  console.log(isReadyToPost);
+
   const onTakePhoto = () => {
     console.log('делаем фото ', Date.now());
   };
 
   const onPost = () => {
+    if (!isReadyToPost) {
+      return;
+    }
     console.log('публикуем фото ', Date.now());
   };
 
   const onDelete = () => {
     console.log('удаляем фото ', Date.now());
+    setPhotoName('');
+    setPhotoLocation('');
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.photoBox}>
-        <TouchableOpacity style={styles.photoBtn} activeOpacity={0.8} onPress={onTakePhoto}>
-          <MaterialIcons name="photo-camera" size={24} color="#BDBDBD" />
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.mainText}>Завантажте фото</Text>
-      <TextInput
-        style={styles.photoName}
-        placeholder="Назва..."
-        placeholderTextColor="#BDBDBD"
-      ></TextInput>
-      <View style={styles.locationWrapper}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? -270 : -220}
+      >
+        <View style={styles.photoBox}>
+          <TouchableOpacity style={styles.photoBtn} activeOpacity={0.8} onPress={onTakePhoto}>
+            <MaterialIcons name="photo-camera" size={24} color="#BDBDBD" />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.mainText}>Завантажте фото</Text>
         <TextInput
-          style={styles.photoLocation}
-          placeholder="Місцевість..."
+          style={styles.photoName}
+          placeholder="Назва..."
           placeholderTextColor="#BDBDBD"
-        ></TextInput>
-        <Feather name="map-pin" size={24} color="#BDBDBD" style={styles.locationIcon} />
-      </View>
-      <TouchableOpacity style={styles.postBtn} activeOpacity={0.8} onPress={onPost}>
-        <Text style={styles.postBtnText}>Опублікувати</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.deleteBtn} activeOpacity={0.8} onPress={onDelete}>
-        <Feather name="trash-2" size={24} color="#BDBDBD" />
-      </TouchableOpacity>
-    </View>
+          value={photoName}
+          onChangeText={setPhotoName}
+        />
+        <View style={styles.locationWrapper}>
+          <TextInput
+            style={styles.photoLocation}
+            placeholder="Місцевість..."
+            placeholderTextColor="#BDBDBD"
+            value={photoLocation}
+            onChangeText={setPhotoLocation}
+          />
+          <Feather name="map-pin" size={24} color="#BDBDBD" style={styles.locationIcon} />
+        </View>
+        <TouchableOpacity
+          style={[styles.postBtn, isReadyToPost && styles.activePostBtn]}
+          activeOpacity={0.8}
+          onPress={onPost}
+        >
+          <Text style={[styles.postBtnText, isReadyToPost && styles.activePostText]}>
+            Опублікувати
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.deleteBtn} activeOpacity={0.8} onPress={onDelete}>
+          <Feather name="trash-2" size={24} color="#BDBDBD" />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -93,6 +131,8 @@ const styles = StyleSheet.create({
 
     fontFamily: 'Roboto-Regular',
     fontSize: 16,
+
+    color: '#212121',
   },
   photoLocation: {
     height: 50,
@@ -105,6 +145,8 @@ const styles = StyleSheet.create({
 
     fontFamily: 'Roboto-Regular',
     fontSize: 16,
+
+    color: '#212121',
   },
   locationWrapper: {
     position: 'relative',
@@ -124,10 +166,16 @@ const styles = StyleSheet.create({
 
     alignItems: 'center',
   },
+  activePostBtn: {
+    backgroundColor: '#FF6C00',
+  },
   postBtnText: {
     color: '#BDBDBD',
     fontFamily: 'Roboto-Regular',
     fontSize: 16,
+  },
+  activePostText: {
+    color: '#fff',
   },
   deleteBtn: {
     width: 70,
